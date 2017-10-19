bef776644052ea1ec4ac80f04cceb652715dca12
MacenDi <mstanekofficial@gmail.com>
2017-10-19 20:35:15 +0200

    Printing account detalis in account fixed

--- controller/SigmaNet.py
+++ controller/SigmaNet.py
@@ -14,10 +14,13 @@ class SigmaNet:
     def Accounts(self):
         """list every account login info"""
         print(self.FbChatManager.fbAccounts)
+        for account in self.FbChatManager.fbAccounts.fbLogins:
+            print("name: " + account)
+            for accountDetalis in self.FbChatManager.fbAccounts.fbLogins[account]:
+                print(accountDetalis, ':', self.FbChatManager.fbAccounts.fbLogins[account][accountDetalis])
 
     def ActiveAccounts(self):
         """list every fb account that sigma is logged in"""
         for client in self.FbChatManager.activeFbClients:
             print("name: "+ client)
             print("behaviour: "+ self.FbChatManager.activeFbClients[client][1])
-
