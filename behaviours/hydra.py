23ec310a1a07b855b91e9bcfe33cad452acca548
MacenDi <mstanekofficial@gmail.com>
2017-10-19 23:18:44 +0200

    Revert "Gitignore added, hydra.py fixed"
    This reverts commit 7f4acbd4d1b22099490671273f9ba2d361f6c73c.

--- behaviours/hydra.py
+++ behaviours/hydra.py
@@ -4,23 +4,33 @@ import threading
 
 from model import behaviourbase
 
-class behaviourClass(behaviourbase.IBehaviourBase):
+SUBJECT_ID="100009816161880"
+CONF_ID="1149408855159567"
+CONSPIRACY=["100007142767580","100006193238725", "100005032903512","100006245283330","100016113762090"]
 
+class behaviourClass(behaviourbase.IBehaviourBase):
+    """behaviour class of  hydra it is loaded by importlib"""
+    addFlag=False
+    addQueue=[] #lista osob ktore maja zostc dodane
+
+    #watki na petle oczekujace
+    HydraThreads=[]
+
+
+    #uproszczona funkcja wysylania wiwaomosci
     def SendToSigma(self, message):
-        self.sendMessage(message, thread_id=self.CONF_ID, thread_type=fbchat.ThreadType.GROUP)
+        self.sendMessage(message, thread_id=CONF_ID, thread_type=fbchat.ThreadType.GROUP)
 
 
     def onPersonRemoved(self, removed_id, author_id, thread_id, **kwargs):
         #dodawaj tylko jezeli czlonek spisku && jezeli czlonek spisku wywalil to nie dodawaj
         #if(removed_id != self.uid and thread_id==CONF_ID and (removed_id in  CONSPIRACY) and (author_id not in CONSPIRACY)):
-
             self.addQueue.append(removed_id)
-            self.SendToSigma("Ej, a to taki dobry człowiek jest :<")
+            self.SendToSigma("elo")
             if(not self.addFlag):
                 self.HydraThreads.append(threading.Thread(target=self.addingLoop))
                 self.HydraThreads[-1].start()
             self.addFlag =True
-
 
             #### usuwanie wykomentowanie dla maksymalizacji rozpaczy
             #Client.removeUserFromGroup(self, user_id=author_id, thread_id=CONF_ID)
@@ -30,17 +40,9 @@ class behaviourClass(behaviourbase.IBehaviourBase):
 
 
     def addingLoop(self):
-        while(self.addQueue.__len__() != 0):
-            Client.addUsersToGroup(self, self.addQueue.pop(), self.CONF_ID)
-
-        self.addFlag = False
-
+        for person in self.addQueue:
+            Client.addUsersToGroup(self, person, CONF_ID)
 
     def Run(self):
-        self.addFlag = False
-        self.addQueue=[]
-        self.HydraThreads = []
-        self.CONF_ID = "1149408855159567"
-        self.SendToSigma("HYDREN STARTEN")
         while(True):
-            self.listen()
+            self.SendToSigma("test123")
